c20bea3153af28db1c55a6a453d35d88a24fb624
Michael <kelle.michael@web.de>
2024-06-24 11:52:25 +0200

    Add Controller interface and implement in HelloController
    The added Controller interface includes an init() method. This is now implemented in HelloController which also includes the addition of a MenuBar.
    
    hello-view.fxml file has been renamed to main-window.fxml.
    
    On Application start the Controller.init() Methods is called

--- src/main/java/devclub/paint/HelloApplication.java
+++ src/main/java/devclub/paint/HelloApplication.java
@@ -1,5 +1,6 @@
 package devclub.paint;
 
+import devclub.paint.interfaces.Controller;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -11,10 +12,14 @@ public class HelloApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("main-window.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1024, 786);
+        stage.setTitle("Paint Application by Dev-Club");
         stage.setScene(scene);
+
+        Controller controller = fxmlLoader.getController();
+        controller.init();
+
         stage.show();
     }
 
--- src/main/java/devclub/paint/HelloController.java
+++ src/main/java/devclub/paint/HelloController.java
@@ -1,14 +1,40 @@
 package devclub.paint;
 
+import devclub.paint.interfaces.Controller;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
 
-public class HelloController {
+public class HelloController implements Controller {
+
+    @FXML
+    private MenuBar menuBar;
+
     @FXML
     private Label welcomeText;
 
+
+    @Override
+    public void init() {
+        addMenuAndMenuItemsToMenubar();
+    }
+
+    /**
+     * Will be deleted in the future, reference for Event-Handling
+     */
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    /**
+     * Adding Menu-Items to the Menubar
+     * @author Michael
+     */
+    private void addMenuAndMenuItemsToMenubar(){
+        menuBar.getMenus().add(new Menu("File"));
+        menuBar.getMenus().add(new Menu("Edit"));
+        menuBar.getMenus().add(new Menu("View"));
     }
 }
